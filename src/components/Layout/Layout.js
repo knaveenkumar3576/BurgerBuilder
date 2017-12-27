75914fbc972fd30850c60883a3f6e3b53dee404c
unknown <knaveenkumar3576@gmail.com>
2017-12-26 17:36:11 -0700

    created burger builder component, css for Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Aux from '../../hoc/Auxillary'
+import classes from './Layout.css'
 
 const Layout=(props) => (
     <Aux>
         <div>
            Toolbar, SideDrawer, Backdrop 
         </div>
-        <main>
+        <main className={classes.content}>
             {props.children}
         </main>
     </Aux>
