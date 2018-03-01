27e41673bc8bdca11e38c37730cec63392ef1e68
Naveen Kumar Krishnasamy <nkrish20@asu.edu>
2018-02-28 22:37:58 -0700

    navigation links changed, Orders page completed

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,13 +2,25 @@ import React from 'react';
 import classes from './Order.css'
 
 const Order = (props) => {
+    const ingredients = []
+    for (let ingredient in props.ingredients) {
+        ingredients.push({
+            name: ingredient,
+            amount: props.ingredients[ingredient]
+        }); 
+    }
+
+    let ingredientsString = ingredients.map(ingredient => {
+        return <span className={classes.Ingredient}> {ingredient.name} ({ingredient.amount})</span>;
+    });
+
     return (
         <div className={classes.Order}>
             <p>
-                Ingredients: Salad (1)
+               Ingredients: {ingredientsString} 
             </p>
             <p>
-                Price: USD 5.4
+                Price: USD {Number.parseFloat(props.price)}
             </p>
         </div>
     )
